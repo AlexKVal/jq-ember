2383f818a23ed2cac2e4a01505a2fbc20357abca
AlexKVal <alexkval@gmail.com>
2015-12-11 18:16:16 +0200

    [added] 'stopEvent' action handler

--- app/components/jquery-pic.js
+++ app/components/jquery-pic.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    stopEvents() {
+      this.get('jqplugin').stop();
+    }
+  },
   didInsertElement() {
     let jquerypic = this.$(".jquerypic").jquerypic();
     this.set('jqplugin', jquerypic);
